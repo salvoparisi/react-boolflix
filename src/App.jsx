9796574a03e238a74a4a45d50ed789cf8d5c085e
salvoparisi <psalvo065@gmail.com>
2024-11-28 22:32:17 +0100

    add prev and next page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Search from './components/Search'
 import MovieList from './components/MovieList'
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   return (
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,27 +2,57 @@ import { useState, useEffect } from 'react'
 
 function MovieList() {
     const [movie, setMovie] = useState([])
-    const url = 'https://api.themoviedb.org/3/search/movie?api_key=43a33bed768e04ff02ede288e6c2b21a&query=matrix';
+    const [page, setPage] = useState(1)
+    const imgUrl = 'https://image.tmdb.org/t/p/w300'
+    const url = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=';
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0M2EzM2JlZDc2OGUwNGZmMDJlZGUyODhlNmMyYjIxYSIsIm5iZiI6MTczMjc5MTA3OC42OTE4MTc1LCJzdWIiOiI2NzQ4MzMyMjJhNTYyMmIxMDU3MTc5MWMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.as_Fmny_qpM_GIx-jHRG0MmdVqMIf8lm_CxWBR-YQSE'
+        }
+    };
+
+
 
     const fetchMovie = () => {
-        fetch(url)
+        const urlPage = url + page
+        fetch(urlPage, options)
             .then(res => res.json())
             .then(json => setMovie(json.results))
             .catch(err => console.error(err));
     }
 
     useEffect(() => {
-        fetchMovie()
-    }, [])
+        fetchMovie(page)
+    }, [page])
 
+    const prevPage = () => {
+        setPage((prev) => prev - 1)
+        console.log(page);
+        fetchMovie()
+    }
+    const nextPage = () => {
+        setPage((prev) => prev + 1)
+        console.log(page);
+        fetchMovie()
+    }
 
     console.log(movie);
 
     return (
-        <div>
+        <div className='container d-flex flex-wrap'>
             {movie.map(film => {
-                return <img src={`${url}${film.poster_path}`} alt="" />
+                return <>
+                    <div className='filmCard'>
+                        <img src={`${imgUrl}${film.poster_path}`} alt="" />
+                        <h4>{film.title}</h4>
+                    </div>
+
+                </>
             })}
+            <button onClick={prevPage}>prev</button>
+            <button onClick={nextPage}>next</button>
         </div>
     )
 }
